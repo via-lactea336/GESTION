305e6a4ca83bf30adeb629efcefade2654984fc3
via-lactea336 <bryan.quintana@fiuni.edu.py>
2024-05-03 12:47:04 -0400

    chore: Update BankAccount component styling and add "Ver Detalles" link

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -1,4 +1,3 @@
-
 import Header from "@/components/dashboard/account/Header";
 import BankAccount from "@/components/dashboard/account/BankAccount";
 
@@ -6,12 +5,28 @@ export default function Page() {
   return (
     <div>
       <Header />
-      
+
       <div className="bg-primary-200 container mx-auto mt-2">
-        <h1 className="text-4xl font-bold text-center mb-5">Cuentas</h1>
+        <h1 className="text-4xl font-bold text-center pt-3 text-primary-900">
+          Cuentas
+        </h1>
         <div className="grid grid-cols-3 gap-4">
-        <BankAccount accountType="Cuenta Corriente" bankName="Banco Familiar" ruc="1234567-8" company="X"  amount={5000} accountId="10" />
-          <BankAccount accountType="Cuenta de Ahorro" bankName="Banco Continental" ruc="7894561-2" company="X" amount={10000} accountId="2" />
+          <BankAccount
+            accountType="Cuenta Corriente"
+            bankName="Banco Familiar"
+            ruc="1234567-8"
+            company="X"
+            amount={5000}
+            accountId="10"
+          />
+          <BankAccount
+            accountType="Cuenta de Ahorro"
+            bankName="Banco Continental"
+            ruc="7894561-2"
+            company="X"
+            amount={10000}
+            accountId="2"
+          />
         </div>
       </div>
     </div>
--- src/components/dashboard/account/BankAccount.tsx
+++ src/components/dashboard/account/BankAccount.tsx
@@ -1,26 +1,37 @@
 type BankAccountProps = {
   accountType: string;
   bankName: string;
-  ruc: string; 
+  ruc: string;
   company: string;
   amount: number;
   accountId: string; // Nuevo prop para el ID de la cuenta
 };
 
-const BankAccount: React.FC<BankAccountProps> = ({ accountType, bankName, ruc, company, amount, accountId }) => {
+const BankAccount: React.FC<BankAccountProps> = ({
+  accountType,
+  bankName,
+  ruc,
+  company,
+  amount,
+  accountId,
+}) => {
   return (
-    <div className="bg-primary-400 p-5 rounded-md shadow-md relative m-12">
+    <div className="bg-primary-400 py-6 px-4 rounded-md shadow-md relative m-12">
       <h2 className="text-lg font-bold">{accountType}</h2>
       <p className="text-sm text-black">{bankName}</p>
-      <p className="text-sm text-black"> {ruc}</p> 
+      <p className="text-sm text-black"> {ruc}</p>
       <p className="text-sm text-black">Empresa {company}</p>
-      <p className="text-xl absolute bottom-10 right-6">{amount} GS</p>
-      <a href={`/dashboard/account/${accountId}`}>
-        <button className="mt-4 bg-primary-600 text-white py-2 px-4 rounded-md">Ver Detalles</button>
-      </a>
+      <div className="flex flex-row-reverse justify-between items-center pt-4 w-full">
+        <p className="text-xl">{amount} GS</p>
+        <a
+          href={`/dashboard/account/${accountId}`}
+          className="bg-primary-600 text-white py-2 px-2 text-sm rounded-md hover:bg-primary-700"
+        >
+          Ver Detalles
+        </a>
+      </div>
     </div>
   );
 };
 
 export default BankAccount;
-
