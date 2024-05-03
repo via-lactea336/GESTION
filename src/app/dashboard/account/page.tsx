8a599159b3e45c36b03ab0c432726f27d379cea1
TuNombreDeUsuario <labelhu2014@gmail.com>
2024-05-03 10:56:32 -0400

    Link ver detalles

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -10,8 +10,8 @@ export default function Page() {
       <div className="bg-primary-200 container mx-auto mt-2">
         <h1 className="text-4xl font-bold text-center mb-5">Cuentas</h1>
         <div className="grid grid-cols-3 gap-4">
-          <BankAccount accountType="Cuenta Corriente" bankName="Banco Familiar" ruc="1234567-8" company="X"  amount={5000} />
-          <BankAccount accountType="Cuenta de Ahorro" bankName="Banco Continental" ruc="7894561-2" company="X" amount={10000} />
+        <BankAccount accountType="Cuenta Corriente" bankName="Banco Familiar" ruc="1234567-8" company="X"  amount={5000} accountId="10" />
+          <BankAccount accountType="Cuenta de Ahorro" bankName="Banco Continental" ruc="7894561-2" company="X" amount={10000} accountId="2" />
         </div>
       </div>
       
