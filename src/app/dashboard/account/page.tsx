b265bca7a9278802ca763bf0440b0849a3869c2c
TuNombreDeUsuario <labelhu2014@gmail.com>
2024-05-03 09:06:21 -0400

    buenas

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -1,10 +1,20 @@
+
 import Header from "@/components/dashboard/account/Header";
+import BankAccount from "@/components/dashboard/account/BankAccount";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-4xl font-bold">Cuentas de Taylor</h1>
+    <div>
       <Header />
+      
+      <div className="bg-primary-200 container mx-auto mt-2">
+        <h1 className="text-4xl font-bold text-center mb-5">Cuentas</h1>
+        <div className="grid grid-cols-3 gap-4">
+          <BankAccount accountType="Cuenta Corriente" bankName="Banco Familiar" ruc="1234567-8" company="X"  amount={5000} />
+          <BankAccount accountType="Cuenta de Ahorro" bankName="Banco Continental" ruc="7894561-2" company="X" amount={10000} />
+        </div>
+      </div>
+      
     </div>
   );
 }
