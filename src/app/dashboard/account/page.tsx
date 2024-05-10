d19c4a06302e3899b0d2ad61a5a0cb7e57500836
TuNombreDeUsuario <labelhu2014@gmail.com>
2024-05-09 23:06:56 -0400

    se filtra por bancos

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -1,32 +1,57 @@
+"use client"
+import React, { useState, useEffect } from "react";
 import Header from "@/components/dashboard/account/Header";
 import BankAccount from "@/components/dashboard/account/BankAccount";
-
+import obtenerCuentaBancaria from "@/lib/cuentaBancaria/obtenerCuentaBancaria";
+import { CuentaBancaria } from "@prisma/client";
 export default function Page() {
-  return (
-    <div>
-      <Header />
+  const [cuentas, setCuentas] = useState<CuentaBancaria[]>([]);
+  const [bancoSeleccionado, setBancoSeleccionado] = useState<string | null>(null);
 
-      <div className="bg-primary-200 container mx-auto mt-2">
+  useEffect(() => {
+    obtenerYMostrarCuentas();
+  }, [bancoSeleccionado]);
+
+  const obtenerYMostrarCuentas = async () => {
+    try {
+      const cuentasData = await obtenerCuentaBancaria();
+     
+      if(cuentasData != undefined && typeof(cuentasData) != "string" && cuentasData.data != undefined && typeof(cuentasData.data) != "string") {
+      console.log(bancoSeleccionado)
+      let cuentasFiltradas = cuentasData.data;
+
+    if (bancoSeleccionado) {
+      cuentasFiltradas = cuentasData.data?.filter((cuenta) => cuenta.bancoId === bancoSeleccionado) ?? cuentasData.data;
+    }
+
+    setCuentas(cuentasFiltradas);}
+
+     
+    } catch (error) {
+      console.error("Error al obtener las cuentas:", error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-full -mt-8">
+         <Header onBancoSeleccionado={setBancoSeleccionado} />
+
+      <div className="bg-primary-200 container mx-auto mt-2 rounded-md">
         <h1 className="text-4xl font-bold text-center pt-3 text-primary-900">
           Cuentas
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-          <BankAccount
-            accountType="Cuenta Corriente"
-            bankName="Banco Familiar"
-            ruc="1234567-8"
-            company="X"
-            amount={5000}
-            accountId="10"
-          />
-          <BankAccount
-            accountType="Cuenta de Ahorro"
-            bankName="Banco Continental"
-            ruc="7894561-2"
-            company="X"
-            amount={10000}
-            accountId="2"
-          />
+        <div className="flex items-center justify-center py-6 flex-wrap gap-4 md:gap-8">
+        {cuentas.map((cuenta: CuentaBancaria) => (
+            <BankAccount
+              key={cuenta.id}
+              id={cuenta.id}
+              numeroCuenta={cuenta.numeroCuenta}
+              esCuentaAhorro={cuenta.esCuentaAhorro}
+              bancoId={cuenta.bancoId}
+              saldo={cuenta.saldo}
+              saldoDisponible={cuenta.saldoDisponible}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/dashboard/account/Header.tsx
+++ src/components/dashboard/account/Header.tsx
@@ -1,40 +1,77 @@
+"use client"
+
 import React from "react";
+import { useEffect, useState } from "react";
+import obtenerBancos from "@/lib/banco/obtenerBancos";
+import { Banco } from "@prisma/client";
 
 type MenuItem = {
   label: string;
   options: string[];
 };
 
-const menu: MenuItem[] = [
-  {
-    label: "Bancos",
-    options: ["Todos","Banco Familiar", "Banco Vision", "Banco ITAU", "Banco Nacional de Fomento"],
-  },
-  {
-    label: "Tipo de cta",
-    options: ["Todos","Cuenta Corriente", "Cuenta de ahorro"],
-  },
-  {
-    label: "Ver saldos",
-    options: ["Todos", "Saldos A", "Saldos B", "Saldos C"],
-  },
-];
+const Header: React.FC<{ onBancoSeleccionado: (bancoId: string) => void }> = ({ onBancoSeleccionado }) => {
+  const estadoInicial= [{id: "0", nombre: "No hay bancos"}]
+  const handleBancoSeleccionado = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const bancoId = event.target.value;
+    onBancoSeleccionado(bancoId);
+  };
+  const obtenerYMostrarBanco = async () => {
+    try {
+      const cuentasData = await obtenerBancos();
+    
 
-const Header: React.FC = () => {
+      if(cuentasData === undefined || typeof(cuentasData) === "string") return 
+      
+      setBanco(cuentasData.data ?? estadoInicial)
+    } catch (error) {
+      console.error("Error al obtener las cuentas:", error);
+    }
+  };
+
+  
+  const [banco,setBanco]= useState<Banco[] >(estadoInicial)
+  useEffect(() => {
+    obtenerYMostrarBanco();
+  }, []);
+
+  const menu: MenuItem[] = [
+    
+    {
+      label: "Tipo de cta",
+      options: ["Todos", "Cuenta Corriente", "Cuenta de ahorro"],
+    },
+  ];
+
   return (
-    <header className="flex justify-between items-center px-3 py-4 md:px-2 bg-primary-800 text-white">
+    <header className="flex gap-3 justify-between items-center flex-wrap px-8 py-4 w-full rounded-md bg-primary-800 text-white">
       <h1 className="text-2xl font-bold">Cuentas</h1>
-      <nav className="flex space-x-4">
+      <nav className="flex flex-wrap  items-center gap-6">  
+        <div className="flex items-center gap-3">
+        <h3 className="mr-2">Bancos</h3>
+        <select className="bg-primary-400 text-white py-1 px-2 rounded-md" onChange={handleBancoSeleccionado}>
+      {banco.map((data,index) =>
+        <option key={index} value={data.id}>{data.nombre}</option>
+      )}
+    </select>
+      </div>
         {menu.map((data, index) => (
-          <div key={index} className="flex items-center">
-            <h3 className="mr-2">{data.label}</h3>
-            <select className="bg-primary-400 text-white py-1 px-2 rounded">
-              {data.options.map((option, i) => (
-                <option key={i}>{option}</option>
-              ))}
-            </select>
-          </div>
+          <div key={index} className="flex items-center gap-3">
+          <h3 className="mr-2">{data.label}</h3>
+          <select className="bg-primary-400 text-white py-1 px-2 rounded-md">
+            {data.options.map((option, i) => (
+              <option key={i}>{option}</option>
+            ))}
+          </select>
+        </div>
         ))}
+        <div className="flex items-center gap-3">
+          <h3>Ver saldos</h3>
+          <input
+            type="checkbox"
+            className="h-4 w-4 accent-primary-500 cursor-pointer"
+          />
+        </div>
       </nav>
     </header>
   );
