27010a3d863003c72feff0d7091671d3667ce775
NicolasCabrera7 <andres.nicolas.cabrera@gmail.com>
2024-05-09 22:03:42 -0400

    PorfaLocoAndaYaQuieroDormir

--- src/app/dashboard/account/[id]/page.tsx
+++ src/app/dashboard/account/[id]/page.tsx
@@ -2,28 +2,32 @@ import obtenerCuentaBancariaPorId from "@/lib/cuentaBancaria/obtenerCuentaBancar
 import obtenerOperaciones from "@/lib/operacion/obtenerOperaciones";
 import Image from "next/image";
 
+
 type Props = {
     params : {id: string}
 }
 
 const  AccountDetailsTab = async ( {params}: Props) => {
-    const accountData = {
-        accountType: "",
-        accountNumber: "",
-        availableBalance: 0,
-        retainedBalance: 0,
-    };
+    const cuentaReq = await obtenerCuentaBancariaPorId(params.id);
+    const operacionesReq = await obtenerOperaciones();
 
-    const cuenta = await obtenerCuentaBancariaPorId(params.id);
-    const operaciones = await obtenerOperaciones();
+    if(cuentaReq === undefined) return <div>Error obteniendo la cuenta</div>;
+    if (typeof(cuentaReq) === "string") return <div>{cuentaReq}</div>;
+    const accountData = cuentaReq.data;
 
-    if(cuenta === undefined) return <div>Error obteniendo la cuenta</div>;
-    if (typeof(cuenta) === "string") return <div>{cuenta}</div>;
+    if(operacionesReq === undefined) return <div>Error obteniendo la cuenta</div>;
+    if (typeof(operacionesReq) === "string") return <div>{operacionesReq}</div>;
+    const operaciones = operacionesReq.data;
 
-    accountData.accountType = cuenta.data?.esCuentaAhorro? "Cuenta de ahorro" : "Cuenta Corriente"; 
-    if(typeof(cuenta.data?.saldo) === "number") accountData.retainedBalance = cuenta.data?.saldo;
-    if(typeof(cuenta.data?.saldoDisponible) === "number") accountData.availableBalance = cuenta.data?.saldoDisponible;
-    if(typeof(cuenta.data?.numeroCuenta) === "string") accountData.accountNumber = cuenta.data?.numeroCuenta;
+    function formatDate(dateString: Date): string {
+        const date = new Date(dateString);
+        const day = date.getDate(); 
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear(); 
+        const formattedDate = `${day < 10 ? '0' + day : day}-${month < 10 ? '0' + month : month}-${year}`;
+    
+        return formattedDate;
+    }
     return (
         <div className="flex flex-col h-full -mt-8">
         {/* Encabezado con título y botón de retroceso */}
@@ -45,18 +49,18 @@ const  AccountDetailsTab = async ( {params}: Props) => {
             className="w-20 h-20 object-cover mr-12 mt-10"
             />
             <div className="flex-1 mr-8 mt-12">
-            <div className="mb-4 flex items-center">
-                <label className="block text-gray-700 font-bold mr-2">
-                Tipo de Cuenta:
-                </label>
-                <p className="text-gray-700">{accountData.accountType}</p>
-            </div>
-            <div className="mb-4 flex items-center">
-                <label className="block text-gray-700 font-bold mr-2">
-                Nombre de la Cuenta:
-                </label>
-                <p className="text-gray-700">{accountData.accountNumber}</p>
-            </div>
+                <div className="mb-4 flex items-center">
+                    <label className="block text-gray-700 font-bold mr-2">
+                    Tipo de Cuenta:
+                    </label>
+                    <p className="text-gray-700">{accountData.esCuentaAhorro? "Cuenta de ahorros" : "Cuenta Corriente"}</p>
+                </div>
+                <div className="mb-4 flex items-center">
+                    <label className="block text-gray-700 font-bold mr-2">
+                    Nombre de la Cuenta:
+                    </label>
+                    <p className="text-gray-700">{accountData.numeroCuenta}</p>
+                </div>
             </div>
 
             <div className="flex-1 mt-12">
@@ -65,7 +69,7 @@ const  AccountDetailsTab = async ( {params}: Props) => {
                 Saldo Disponible:
                 </label>
                 <p className="text-gray-700">
-                ${accountData.availableBalance.toFixed(2)}
+                ${accountData.saldo.toFixed(2)}
                 </p>
             </div>
             <div className="flex items-center">
@@ -73,7 +77,7 @@ const  AccountDetailsTab = async ( {params}: Props) => {
                 Saldo Retenido:
                 </label>
                 <p className="text-gray-700">
-                ${accountData.retainedBalance.toFixed(2)}
+                ${accountData.saldoDisponible.toFixed(2)}
                 </p>
             </div>
             </div>
@@ -81,93 +85,58 @@ const  AccountDetailsTab = async ( {params}: Props) => {
         <h1 className="text-3xl font-bold mt-10">Movimientos</h1>
         <div className="flex-grow bg-primary-200 shadow-md border-2 border-black p-10 flex flex-row">
             <table className="border-collapse w-full">
-            <tbody>
-                <tr>
-                <td>
-                    <h1 className="text-xl font-bold mt-1 text-black">Operacion</h1>
-                </td>
-                <td>
-                    <h1 className="text-xl font-bold mt-1 text-black">Fecha</h1>
-                </td>
-                <td>
-                    <h1 className="text-xl font-bold mt-1 text-black">
-                    Descripcion
-                    </h1>
-                </td>
-                <td>
-                    <h1 className="text-xl font-bold mt-1 text-black">
-                    Monto Transaccion
-                    </h1>
-                </td>
-                <td>
-                    <h1 className="text-xl font-bold mt-1 text-black">
-                    Saldo Actual
-                    </h1>
-                </td>
-                </tr>
-                <tr>
-                <td>
-                    <Image
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6lo6gclz5yoqrqK6NRz4qp5T2NDyBgcDfFx-IxBUTew&s"
-                    alt="Account"
-                    width={10}
-                    height={10}
-                    className="w-10 h-10 object-cover rounded-full bg-violet-400 mt-5 mb-5 ml-5"
-                    />
-                </td>
-                <td>
-                    <h1 className="text-base font-normal mt-1 text-black">
-                    10-02-2024
-                    </h1>
-                </td>
-                <td>
-                    <h1 className="text-base font-normal mt-1 text-black">
-                    Inserse Descripcion aqui
-                    </h1>
-                </td>
-                <td>
-                    <h1 className="text-xl font-bold mt-1 text-green-500">
-                    +200.00$
-                    </h1>
-                </td>
-                <td>
-                    <h1 className="text-base font-normal mt-1 text-black">
-                    3000.00$
-                    </h1>
-                </td>
-                </tr>
-                <tr>
-                <td>
-                    <Image
-                    src="https://c1.klipartz.com/pngpicture/658/794/sticker-png-banking-arrow-money-bank-account-wire-transfer-electronic-funds-transfer-demand-deposit-mobile-banking-stanbic-ibtc-bank.png"
-                    alt="Account"
-                    width={10}
-                    height={10}
-                    className="w-10 h-10 object-cover rounded-full bg-violet-400 mt-5 mb-5 ml-5"
-                    />
-                </td>
-                <td>
-                    <h1 className="text-base font-normal mt-1 text-black">
-                    10-02-2024
-                    </h1>
-                </td>
-                <td>
-                    <h1 className="text-base font-normal mt-1 text-black">
-                    Inserse Descripcion aqui
-                    </h1>
-                </td>
-                <td>
-                    <h1 className="text-xl font-bold mt-1 text-red-500">
-                    -200.00$
-                    </h1>
-                </td>
-                <td>
-                    <h1 className="text-base font-normal mt-1 text-black">
-                    2800.00$
-                    </h1>
-                </td>
-                </tr>
-            </tbody>
+                <tbody>
+                    <tr>
+                        <td className="w-1/4">
+                            <h1 className="text-xl font-bold mt-1 text-black">Operacion</h1>
+                        </td>
+                        <td className="w-1/4">
+                            <h1 className="text-xl font-bold mt-1 text-black">Fecha</h1>
+                        </td >
+                        <td className="w-1/4">
+                            <h1 className="text-xl font-bold mt-1 text-black">
+                            Descripcion
+                            </h1>
+                        </td>
+                        <td className="w-1/4">
+                            <h1 className="text-xl font-bold mt-1 text-black">
+                            Monto Transaccion
+                            </h1>
+                        </td>
+                    </tr>
+                    {operaciones.map((operacion, index) => (
+                    <tr key={index}>
+                        <td>
+                            <img
+                                src={operacion.tipoOperacionId === 'b30dde92-8757-4886-9388-be7dbc6cc275' ? 
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6lo6gclz5yoqrqK6NRz4qp5T2NDyBgcDfFx-IxBUTew&s' :
+                                'https://c1.klipartz.com/pngpicture/658/794/sticker-png-banking-arrow-money-bank-account-wire-transfer-electronic-funds-transfer-demand-deposit-mobile-banking-stanbic-ibtc-bank.png'
+                                }
+                                alt="Account"
+                                width={40}
+                                height={40}
+                                className="w-10 h-10 object-cover rounded-full bg-violet-400 mt-5 mb-5 ml-5"
+                            />
+                        </td>
+                        <td>
+                            <h1 className="text-base font-normal mt-1 text-black">
+                                {formatDate(operacion.fechaOperacion)}
+                            </h1>
+                        </td>
+                        <td>
+                            <h1 className="text-base font-normal mt-1 text-black">
+                                {operacion.concepto}
+                            </h1>
+                        </td>
+                        <td>
+                            <h1 className={`text-xl font-bold mt-1 ${operacion.tipoOperacionId === 'b30dde92-8757-4886-9388-be7dbc6cc275' ? 
+                                'text-green-500' : 'text-red-500'}`}>
+                                {operacion.monto >= 0 ? `+${operacion.monto.toFixed(2)}$` : `${operacion.monto.toFixed(2)}$`}
+                            </h1>
+                        </td>
+                    </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
         </div>
