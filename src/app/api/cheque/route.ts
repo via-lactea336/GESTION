30215ee088f39577749e2b7c023b96a435ac17af
via-lactea336 <bryan.quintana@fiuni.edu.py>
2024-05-14 09:38:16 -0400

    refactor: Update cheque route to include involucrado information in POST request

--- src/app/api/cheque/route.ts
+++ src/app/api/cheque/route.ts
@@ -11,12 +11,15 @@ import { estadoCheque } from "@prisma/client";
 
 export async function POST(req: NextRequest) {
   const body: Cheque = await req.json();
+  const involucrado = {
+    nombre: body.involucradoNombre,
+    cedula: body.involucradoDocumentoIdentidad,
+  };
   const {
     esRecibido,
     numeroCheque,
     monto,
     fechaEmision,
-    involucrado,
     bancoChequeId,
     cuentaBancariaAfectadaId,
   } = body;
@@ -29,7 +32,7 @@ export async function POST(req: NextRequest) {
     involucrado,
     bancoChequeId,
     cuentaBancariaAfectadaId
-  )
+  );
 
   if (
     !numeroCheque ||
@@ -44,19 +47,28 @@ export async function POST(req: NextRequest) {
       "No existe informacion suficiente para generar el registro del cheque",
       400
     ); //Validate data
-    
+
   //Obtiene la cuenta bactaria que se vera afectada en la operacion
   const cuentaBancariaAfectada = await prisma.cuentaBancaria.findUnique({
     where: {
-      id: cuentaBancariaAfectadaId
-    }, include:{
-      entidad: true
-    }
-  })
-  if(!cuentaBancariaAfectada) throw new Error("No existe la cuenta la cual se vera afectada por esta operacion")
+      id: cuentaBancariaAfectadaId,
+    },
+    include: {
+      entidad: true,
+    },
+  });
+  if (!cuentaBancariaAfectada)
+    throw new Error(
+      "No existe la cuenta la cual se vera afectada por esta operacion"
+    );
 
-
-  if(involucrado.trim() === cuentaBancariaAfectada.entidad.nombre.trim()) return generateApiErrorResponse("El emisor del cheque y el acreedor no pueden ser iguales", 400)
+  if (
+    involucrado.nombre.trim() === cuentaBancariaAfectada.entidad.nombre.trim()
+  )
+    return generateApiErrorResponse(
+      "El emisor del cheque y el acreedor no pueden ser iguales",
+      400
+    );
 
   if (Number(monto) <= 0 || !Number.isInteger(Number(monto)))
     return generateApiErrorResponse("Monto invalido", 400);
@@ -66,13 +78,13 @@ export async function POST(req: NextRequest) {
     !esRecibido &&
     Number(cuentaBancariaAfectada.saldoDisponible) - Number(monto) <= 0
   )
-    throw new Error("Saldo disponible insuficiente para realizar la operacion")
-  
+    throw new Error("Saldo disponible insuficiente para realizar la operacion");
+
   if (!esRecibido && Number(cuentaBancariaAfectada.saldo) - Number(monto) <= 0)
-    throw new Error("Saldo retenido insuficiente para realizar la operacion")
+    throw new Error("Saldo retenido insuficiente para realizar la operacion");
 
   //Se obitene el id del banco de la cuenta afectada
-  const bancoCuentaAfectada = cuentaBancariaAfectada.bancoId
+  const bancoCuentaAfectada = cuentaBancariaAfectada.bancoId;
 
   try {
     const cheque = await prisma.cheque.create({
@@ -83,17 +95,30 @@ export async function POST(req: NextRequest) {
         fechaEmision,
         //En caso de que el cheque sea uno que nos depositen a nosotros (la empresa) y que el cheque sea del mismo banco,
         //Entonces eso significa que el cheque esta cobrado y que afecta a nuestros saldos retenido y disponible
-        fechaPago: (esRecibido && bancoChequeId === bancoCuentaAfectada)? fechaEmision : null,
-        involucrado,
-        estado: (esRecibido && bancoChequeId === bancoCuentaAfectada)? estadoCheque.PAGADO : undefined,
+        fechaPago:
+          esRecibido && bancoChequeId === bancoCuentaAfectada
+            ? fechaEmision
+            : null,
+        involucradoNombre: involucrado.nombre,
+        involucradoDocumentoIdentidad: involucrado.cedula,
+        estado:
+          esRecibido && bancoChequeId === bancoCuentaAfectada
+            ? estadoCheque.PAGADO
+            : undefined,
         bancoChequeId,
         cuentaBancariaAfectadaId,
-      }
-    })
+      },
+    });
 
     if (!cheque) return generateApiErrorResponse("Error generando cheque", 400);
 
-    await reflejarChequeRecibido(monto, bancoCuentaAfectada, cuentaBancariaAfectadaId, bancoChequeId, esRecibido)
+    await reflejarChequeRecibido(
+      monto,
+      bancoCuentaAfectada,
+      cuentaBancariaAfectadaId,
+      bancoChequeId,
+      esRecibido
+    );
 
     return generateApiSuccessResponse(200, "cheque agregado correctamente");
   } catch (err) {
