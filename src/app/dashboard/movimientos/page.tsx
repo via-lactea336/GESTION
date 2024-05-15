d015ac839193ac5c4bc1103992e1a791de54a2ba
via-lactea336 <bryan.quintana@fiuni.edu.py>
2024-05-15 11:09:43 -0400

    refactor: Remove unused code and update navigation links

--- src/app/dashboard/movimientos/page.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "@/components/global/Header";
-
-export default function MovimientosPage() {
-  return (
-    <>
-      <Header title="Movimiento" className="-mt-8" />
-    </>
-  );
-}
