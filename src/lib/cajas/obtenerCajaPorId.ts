7351246f42541fdef42a9e6eca09c5550e72dd7d
via-lactea336 <bryan.quintana@fiuni.edu.py>
2024-05-30 19:38:34 -0400

    chore: Refactor authentication options for improved readability and error handling

---
+++ src/lib/cajas/obtenerCajaPorId.ts
@@ -0,0 +1,26 @@
+import { Caja } from "@prisma/client";
+import { ApiResponseData } from "../definitions";
+
+/**
+ * Obtiene una caja de la base de datos
+ * @param id el id de la caja a obtener
+ * @returns la caja obtenida
+ */
+
+export default async function obtenerCajaPorId(id: string) {
+  const server_url = process.env.URL;
+  const url = server_url || "";
+  try {
+    const caja = await fetch(`${url}/api/caja/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data: ApiResponseData<Caja> = await caja.json();
+    if (data.error) throw new Error(data.error);
+    if (!data.data) throw new Error("Error al obtener la caja");
+    return data;
+  } catch (err) {
+    if (err instanceof Error) return err.message;
+  }
+}
