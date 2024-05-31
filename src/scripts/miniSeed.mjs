5c43b13add3dcf6310e6f12dc1470067d8868445
via-lactea336 <bryan.quintana@fiuni.edu.py>
2024-05-31 14:42:32 -0400

    chore: Add lazy loading for images and fix bug in ListaDeCajas component

--- src/scripts/miniSeed.mjs
+++ src/scripts/miniSeed.mjs
@@ -3,31 +3,93 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    const caja1 = await prisma.caja.create({
-        data: {
-            numero: 1,
-            deleted: null,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        },
-    });
 
-    const caja2 = await prisma.caja.create({
-        data: {
-            numero: 2,
-            deleted: null,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        },
-    });
-
-    const caja3 = await prisma.caja.create({
-        data: {
-            numero: 3,
-            deleted: null,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        },
+    // productos de ferreteria
+    const productos = await prisma.producto.createMany({
+        data: [
+            {
+                nombre:"Martillo de carpintero",
+                precio: 250000,
+                codigo: "001",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deleted:null,
+            },
+            {
+                nombre:"Taladro inalambrico",
+                precio: 750000,
+                codigo: "002",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deleted:null,
+            },
+            {
+                nombre:"Cinta metrica",
+                precio: 50000,
+                codigo: "003",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deleted:null,
+            },
+            {
+                nombre:"Caja de herramientas",
+                precio: 350000,
+                codigo: "004",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deleted:null,
+            },
+            {
+                nombre:"Sierra electrica",
+                precio: 450000,
+                codigo: "005",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deleted:null,
+            },
+            {
+                nombre:"Llave de tubo",
+                precio: 150000,
+                codigo: "006",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deleted:null,
+            },
+            {
+                nombre:"Destornillador",
+                precio: 75000,
+                codigo: "007",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deleted:null,
+            },
+            {
+                nombre:"Clavos de acero",
+                precio: 20000,
+                codigo: "008",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deleted:null,
+            },
+            {
+                nombre:"Tornillos de acero",
+                precio: 25000,
+                codigo: "009",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deleted:null,
+            },
+            {
+                nombre:"Pegamento de contacto",
+                precio: 30000,
+                codigo: "010",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deleted:null,
+            
+            },
+        ],
+        skipDuplicates: true,
     });
 
 }
