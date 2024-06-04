b37e449b398babb6ff627be1bf832e515952d844
via-lactea336 <bryan.quintana@fiuni.edu.py>
2024-06-04 15:30:31 -0400

    .

--- src/components/global/Modal.tsx
+++ src/components/global/Modal.tsx
@@ -1,36 +1,25 @@
-"use client";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { useRouter } from "next/router";
-import { Toaster, toast } from "sonner";
 
 type ModalProps = {
   children: React.ReactNode;
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
-  isBryan?: boolean;
 };
 
-export function Modal({ children, setShowModal, isBryan = true }: ModalProps) {
-  const router = useRouter();
+export function Modal({ children, setShowModal }: ModalProps) {
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setShowModal(false);
   };
-  const handleRedirect = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    toast.message("Caja cerrada correctamente");
-    router.push("/dashboard/caja");
-  };
   return (
     <div className="w-1/2 z-50 relative mx-auto align-middle h-full p-12 bg-gray-700 rounded-md">
       <button
-        onClick={isBryan ? handleClick : handleRedirect}
+        onClick={handleClick}
         className="absolute right-4 top-4"
         title="cerrar"
       >
         <XMarkIcon className="text-white  w-7 h-7 hover:text-primary-400" />
       </button>
       <div>{children}</div>
-      <Toaster richColors />
     </div>
   );
 }
