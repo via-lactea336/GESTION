386f1e7f3c2b062f403d7ffba1b96615abdc2814
via-lactea336 <bryan.quintana@fiuni.edu.py>
2024-06-04 15:53:48 -0400

    chore: Update cerrarCajaAdmin.ts to use server URL from environment variable

--- src/lib/aperturaCaja/cerrarCajaAdmin.ts
+++ src/lib/aperturaCaja/cerrarCajaAdmin.ts
@@ -1,14 +1,16 @@
-import { fetchPlus } from "../verificarApiResponse"
+import { fetchPlus } from "../verificarApiResponse";
 
-const cerrarCajaAdmin = async (cajaId:string, aperturaId:string) => {
-  return await fetchPlus(`http://localhost:3000/api/caja/${cajaId}/cerrar`, {
+const cerrarCajaAdmin = async (cajaId: string, aperturaId: string) => {
+  const server_url = process.env.URL;
+  const url = server_url || "";
+  return await fetchPlus(`${url}/api/caja/${cajaId}/cerrar`, {
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      aperturaId
+      aperturaId,
     }),
     method: "PUT",
-  })
-}
-export default cerrarCajaAdmin
+  });
+};
+export default cerrarCajaAdmin;
