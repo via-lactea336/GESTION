06cfd22b8aa5aa6eee9712356d4baea40cd8c870
TuNombreDeUsuario <labelhu2014@gmail.com>
2024-06-04 20:44:17 -0400

    pantallaResumen sin conectar a la api

--- src/app/dashboard/caja/[id]/(cierreDeCaja)/resumenDiario/page.tsx
+++ src/app/dashboard/caja/[id]/(cierreDeCaja)/resumenDiario/page.tsx
@@ -1,33 +1,43 @@
 import Header from "@/components/global/Header";
 import ResumenDeCaja from "@/components/dashboard/resumendiario/ResumenDeCaja";
-import TablaResumen from "@/components/dashboard/resumendiario/TablaResumen";
+import { Decimal } from '@prisma/client/runtime/library';
+import { RegistroCaja } from '@prisma/client';
+
+import { fetchPlus } from "@/lib/verificarApiResponse";
+
+
 
 export default function Page() {
-    return (
-      <div className="flex flex-col h-full -mt-8">
-        <Header title="Resumen diario" />
-        
-        <div className="bg-gray-800 py-6 px-4 rounded-md shadow-md "> 
-          <div className="flex justify-center gap-8" > {/* Ajusta el espaciado horizontal */}
+
+  const registroCaja: RegistroCaja = {
+    id: "1",
+    aperturaId: "Cerrado",
+    montoInicial: new Decimal(1000),
+    montoRegistrado: new Decimal(700000),
+    montoEsperado: new Decimal(600000),
+    cantCheques: 5,
+    cantTarjetas: 3,
+    montoIngreso: new Decimal(900000),
+    montoEgreso: new Decimal(200000),
+    montoIngresoCheque: new Decimal(400000),
+    montoEgresoCheque: new Decimal(100000),
+    montoIngresoTarjeta: new Decimal(300000),
+    montoEgresoTarjeta: new Decimal(50000),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deleted: null
+  };
+
+  return (
+    <div className="flex flex-col h-full -mt-8">
+      <Header title="Resumen diario" />
+      <div className="bg-gray-800 py-6 px-4 rounded-md shadow-md"> 
+        <div className="flex justify-center gap-8">
           <div className="w-full">
-            <ResumenDeCaja
-              estado="Cerrado"
-              cajaInicial={100000}
-              dineroEnCaja={700000}
-              diferencia={600000}
-              fecha="27/05/2024" 
-              caja="1"
-              
-            />
-            </div>
-            
+            <ResumenDeCaja {...registroCaja} />
           </div>
-          <TablaResumen
-          efectivo= {100000}
-          cheque={200000}
-          deposito={100000}/>
-    
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
