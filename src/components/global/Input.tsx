7093a310acee46e48fb0f3875cc5f4add65dc0bc
via-lactea336 <bryan.quintana@fiuni.edu.py>
2024-06-09 17:51:01 -0400

    Refactor Input component to handle formatted numbers and disabled state

--- src/components/global/Input.tsx
+++ src/components/global/Input.tsx
@@ -26,36 +26,52 @@ export default function Input({
   disabled,
 }: InputProps) {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "-" || e.key === "+" || e.key === "e") {
+    // Bloquear caracteres no permitidos
+    if (
+      !/[0-9]/.test(e.key) &&
+      e.key !== "Backspace" &&
+      e.key !== "Tab" &&
+      e.key !== "ArrowLeft" &&
+      e.key !== "ArrowRight" &&
+      e.key !== "Delete"
+    ) {
       e.preventDefault();
     }
   };
 
   const handleFormattedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const rawValue = e.target.value.replace(/\D/g, "");
-    const formattedValue = rawValue.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    if (onChange) {
-      e.target.value = formattedValue;
-      onChange(e);
+    const rawValue = e.target.value.replace(/\./g, "");
+
+    // Validar que el valor es un número
+    if (/^\d*$/.test(rawValue)) {
+      onChange && onChange({ ...e, target: { ...e.target, value: rawValue } });
     }
   };
+
+  const formatted =
+    value === 0 ? "" : (value as number).toLocaleString("es-PY");
+  const val = type === "formattedNumber" ? formatted : value;
 
   return (
     <div>
       <input
         className={`[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none ${className} ${
-          error && "border border-red-500"
-        } ${disabled && "cursor-not-allowed"}`}
+          error ? "border border-red-500" : ""
+        } ${disabled ? "cursor-not-allowed" : ""}`}
         id={id}
         name={id}
         type={type === "formattedNumber" ? "text" : type}
         placeholder={disabled ? "" : placeholder}
         disabled={disabled || false}
-        value={value}
+        value={val}
         required={required || false}
         min={type === "number" ? 1 : undefined}
         onChange={type === "formattedNumber" ? handleFormattedChange : onChange}
-        onKeyDown={type === "number" ? handleKeyDown : onkeydown}
+        onKeyDown={
+          type === "formattedNumber" || type === "number"
+            ? handleKeyDown
+            : onkeydown
+        }
       />
       {error && (
         <p title="mensaje de error" className="text-gray-200 text-sm">
