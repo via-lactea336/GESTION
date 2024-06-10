d9d4d93385760a1e0ed96ada5b71c3b5075b42f7
via-lactea336 <bryan.quintana@fiuni.edu.py>
2024-06-10 10:58:32 -0400

    chore: Update page header and add navigation links

--- src/app/dashboard/caja/page.tsx
+++ src/app/dashboard/caja/page.tsx
@@ -2,13 +2,27 @@ import ContenedorCajas from "@/components/dashboard/caja/ContenedorCajas";
 import Header from "@/components/global/Header";
 import authOptions from "@/lib/auth/options";
 import { getServerSession } from "next-auth";
+import Link from "next/link";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
+  const links = [
+    { href: `/dashboard/caja`, text: "Inicio" },
+    { href: `/dashboard/caja/reportes`, text: "Reportes" },
+  ];
 
   return (
     <div className="relative">
-      <Header title="Punto de venta" className="-mt-8">
+      <Header title="Apertura De Caja" className="-mt-8">
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            className="bg-gray-800 py-1 px-2 rounded-md hover:text-primary-100 hover:bg-gray-900 mx-2"
+          >
+            {link.text}
+          </Link>
+        ))}
         <span>{session ? session.user.nombre : "Desconocido"}</span>
         <span>
           {new Date()
