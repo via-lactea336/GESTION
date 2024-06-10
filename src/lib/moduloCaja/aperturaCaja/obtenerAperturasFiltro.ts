d2ede1906afb95084830c77424580e78c6133173
Agus3160 <ampharos321@hotmail.com>
2024-06-10 12:01:54 -0400

    Se agregaron funcionalidades a aperturaId y a registroCaja (API)

---
+++ src/lib/moduloCaja/aperturaCaja/obtenerAperturasFiltro.ts
@@ -0,0 +1,33 @@
+import { DatosFiltrados } from "@/lib/definitions";
+import { fetchPlus } from "@/lib/verificarApiResponse";
+import { AperturaCaja, User } from "@prisma/client";
+
+export type Filtro = {
+  cajaId?: string,
+  cerrarda?: boolean,
+  fechaDesde?: string,
+  fechaHasta?: string,
+  skip:number,
+  upTo:number
+}
+
+type Data = AperturaCaja & {cajero: {nombre:string, apellido:string} & {registro: {id:string}}}
+
+export default async function obtenerAperturasFiltro({fechaDesde, fechaHasta, cajaId, skip, upTo}:Filtro) {
+  const server_url = process.env.URL;
+  const url = server_url || "";
+
+  const searchParams = new URLSearchParams()
+
+  if(fechaDesde) searchParams.append('fechaDesde', fechaDesde)
+  if(fechaHasta) searchParams.append('fechaHasta', fechaHasta)
+  if(cajaId) searchParams.append('cajaId', cajaId)
+
+  if(skip) searchParams.append('skip', skip.toString())
+  if(upTo) searchParams.append('upTo', upTo.toString())
+  
+  const queryString = searchParams.toString()
+
+  return await fetchPlus<DatosFiltrados<Data>>(`${url}/api/apertura-caja/search?${queryString}`)
+
+}
--- src/lib/moduloCaja/resumenDiario/obtenerRegistroDeCajaPorAperturaId.ts
+++ src/lib/moduloCaja/resumenDiario/obtenerRegistroDeCajaPorAperturaId.ts
@@ -1,10 +1,10 @@
-import { DatosExtendidosRegistroCaja } from "@/lib/definitions";
+import { RegistroDiarioFullData } from "@/lib/definitions";
 import { fetchPlus } from "@/lib/verificarApiResponse";
 
 export default async function obtenerRegistroDeCajaPorAperturaId(aperturaId: string) {
   const server_url = process.env.URL;
   const url = server_url || "http://localhost:3000";
-  const response = await fetchPlus<DatosExtendidosRegistroCaja>(`${url}/api/registro-caja/apertura/${aperturaId}`, {
+  const response = await fetchPlus<RegistroDiarioFullData>(`${url}/api/registro-caja/apertura/${aperturaId}`, {
     method: "GET",
   });
   return response
