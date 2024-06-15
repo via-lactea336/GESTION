1eae806d3729eba41a54cea7408909dee5857c44
NicolasCabrera7 <andres.nicolas.cabrera@gmail.com>
2024-06-15 12:35:11 -0400

    Tiene un error pero igual imprime no se porque xd odio esto mmmm

--- src/components/PDF/ResumenDiario.tsx
+++ src/components/PDF/ResumenDiario.tsx
@@ -9,6 +9,7 @@ import {
 } from "@react-pdf/renderer";
 import { saveAs } from "file-saver";
 import { Movimiento } from "@prisma/client";
+import { movimientoDetallado } from "../cajaVentanasEmergentes/ResumenDetalle";
 
 // Definir los estilos
 const styles = StyleSheet.create({
@@ -35,6 +36,7 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
     },
     table: {
+        display: "table",
         width: "auto",
         marginBottom: 10,
     },
@@ -49,26 +51,55 @@ const styles = StyleSheet.create({
     },
     tableCell: {
         margin: 5,
+        padding: 5,
         fontSize: 12,
     },
     tableHeader: {
         backgroundColor: "#D3D3D3",
         fontWeight: "bold",
     },
+    movimientoSection: {
+        backgroundColor: "#f8f8f8",
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 5,
+    },
+    movimientoHeader: {
+        fontSize: 16,
+        textAlign: "center",
+        marginBottom: 10,
+    },
+    movimientoDetails: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 10,
+        flexWrap: "wrap",
+    },
+    detalle: {
+        marginBottom: 5,
+        flexWrap: "wrap",
+    },
+    textContainer: {
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+    },
 });
 
 type ResumenCajaProps = {
     createdAt: Date;
     caja: number;
-    cajero: string ;
+    cajero: string;
     apertura: { saldoInicial: number, createdAt: Date };
     montoRegistrado: number;
     montoIngreso: number;
     montoIngresoCheque: number;
     montoIngresoTarjeta: number;
-    movimientos: Array<Movimiento>;
+    movimientos: Array<movimientoDetallado>;
     observaciones: string;
 };
+
+const formatDate = (date: Date) => new Date(date).toLocaleDateString();
+const formatTime = (date: Date) => new Date(date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
 // Componente del Resumen de Caja PDF
 const ResumenCajaPDF: React.FC<ResumenCajaProps> = ({
@@ -83,44 +114,43 @@ const ResumenCajaPDF: React.FC<ResumenCajaProps> = ({
     movimientos,
     observaciones
 }) => (
-    <Document>
-        <Page size="A4" style={styles.page}>
+<Document>
+    <Page size="A4" style={styles.page}>
         <View style={styles.section}>
             <Text style={styles.header}>Resumen de Caja</Text>
             <Text style={styles.text}>
             <Text style={styles.bold}>Fecha: </Text>
-            {new Date(createdAt).toLocaleDateString()}
+                {formatDate(createdAt)}
             </Text>
             <Text style={styles.text}>
             <Text style={styles.bold}>Hora: </Text>
-            {new Date(createdAt).getHours()}:{new Date(createdAt).getMinutes()}
+                {formatTime(createdAt)}
             </Text>
             <Text style={styles.text}>
             <Text style={styles.bold}>Caja: </Text>
-            {caja}
+                {caja}
             </Text>
             <Text style={styles.text}>
             <Text style={styles.bold}>Cajero: </Text>
-            {cajero}
+                {cajero}
             </Text>
-
             <View style={styles.table}>
                 <View style={[styles.tableRow]}>
                     <Text style={[styles.tableCol, styles.tableCell]}>Caja Inicial</Text>
                     <Text style={[styles.tableCol, styles.tableCell]}>
-                    {Number(apertura?.saldoInicial).toLocaleString()} Gs.
+                        {Number(apertura?.saldoInicial).toLocaleString()} Gs.
                     </Text>
                 </View>
                 <View style={styles.tableRow}>
                     <Text style={[styles.tableCol, styles.tableCell]}>Dinero en Caja</Text>
                     <Text style={[styles.tableCol, styles.tableCell]}>
-                    {Number(montoRegistrado).toLocaleString()} Gs.
+                        {Number(montoRegistrado).toLocaleString()} Gs.
                     </Text>
                 </View>
                 <View style={styles.tableRow}>
                     <Text style={[styles.tableCol, styles.tableCell]}>Observaciones</Text>
                     <Text style={[styles.tableCol, styles.tableCell]}>
-                    {observaciones}
+                        {observaciones}
                     </Text>
                 </View>
             </View>
@@ -128,81 +158,77 @@ const ResumenCajaPDF: React.FC<ResumenCajaProps> = ({
             <Text style={styles.header}>Resumen de ingresos y egresos por forma de pago</Text>
 
             <View style={styles.table}>
-            <View style={[styles.tableRow, styles.tableHeader]}>
-                <Text style={[styles.tableCol, styles.tableCell]}>Forma de Pago</Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>Ingreso</Text>
+                <View style={[styles.tableRow, styles.tableHeader]}>
+                    <Text style={[styles.tableCol, styles.tableCell]}>Forma de Pago</Text>
+                    <Text style={[styles.tableCol, styles.tableCell]}>Ingreso</Text>
+                </View>
+                <View style={styles.tableRow}>
+                    <Text style={[styles.tableCol, styles.tableCell]}>Efectivo</Text>
+                    <Text style={[styles.tableCol, styles.tableCell]}>
+                        {Number(montoIngreso).toLocaleString()} Gs.
+                    </Text>
+                </View>
+                <View style={styles.tableRow}>
+                    <Text style={[styles.tableCol, styles.tableCell]}>Cheque</Text>
+                    <Text style={[styles.tableCol, styles.tableCell]}>
+                        {Number(montoIngresoCheque).toLocaleString()} Gs.
+                    </Text>
+                </View>
+                <View style={styles.tableRow}>
+                    <Text style={[styles.tableCol, styles.tableCell]}>Tarjetas</Text>
+                    <Text style={[styles.tableCol, styles.tableCell]}>
+                        {Number(montoIngresoTarjeta).toLocaleString()} Gs.
+                    </Text>
+                </View>
             </View>
-            <View style={styles.tableRow}>
-                <Text style={[styles.tableCol, styles.tableCell]}>Efectivo</Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>
-                {Number(montoIngreso).toLocaleString()} Gs.
-                </Text>
-            </View>
-            <View style={styles.tableRow}>
-                <Text style={[styles.tableCol, styles.tableCell]}>Cheque</Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>
-                {Number(montoIngresoCheque).toLocaleString()} Gs.
-                </Text>
-            </View>
-            <View style={styles.tableRow}>
-                <Text style={[styles.tableCol, styles.tableCell]}>Tarjetas</Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>
-                {Number(montoIngresoTarjeta).toLocaleString()} Gs.
-                </Text>
-            </View>
-            </View>
-
             <Text style={styles.header}>Movimientos</Text>
-
-            <View style={styles.table}>
-            <View style={[styles.tableRow, styles.tableHeader]}>
-                <Text style={[styles.tableCol, styles.tableCell]}>Operacion</Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>Monto</Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>Fecha</Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>Hora</Text>
-            </View>
-            <View style={styles.tableRow}>
-                <Text style={[styles.tableCol, styles.tableCell, { color: "green" }]}>
-                Apertura de caja
-                </Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>
-                {Number(apertura.saldoInicial).toLocaleString()} Gs.
-                </Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>
-                {new Date(apertura.createdAt).toLocaleDateString()}
-                </Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>
-                {new Date(apertura.createdAt).getHours()}:
-                {new Date(apertura.createdAt).getMinutes()}
-                </Text>
-            </View>
+            
             {movimientos?.map((mov) => (
-                <View style={styles.tableRow} key={mov.id}>
-                <Text
-                    style={[
-                    styles.tableCol,
-                    styles.tableCell,
-                    { color: mov.esIngreso ? "green" : "red" },
-                    ]}
-                >
-                    {mov.esIngreso ? "Ingreso" : "Egreso"}
-                </Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>
-                    {Number(mov.monto).toLocaleString()} Gs.
-                </Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>
-                    {new Date(mov.createdAt).toLocaleDateString()}
-                </Text>
-                <Text style={[styles.tableCol, styles.tableCell]}>
-                    {new Date(mov.createdAt).getHours()}:
-                    {new Date(mov.createdAt).getMinutes()}
-                </Text>
+                <View style={styles.movimientoSection} key={mov.id}>
+                    <Text style={styles.movimientoHeader}>
+                        {mov.esIngreso ? "Operacion de Ingreso" : "Operacion de Egreso"}
+                    </Text>
+                    <View style={styles.movimientoDetails}>
+                        <View>
+                            <Text style={[styles.detalle, styles.textContainer]}>Monto total de la operacion: {Number(mov.monto).toLocaleString()} Gs.</Text>
+                            <Text style={[styles.detalle, styles.textContainer]}>Fecha: {formatDate(mov.createdAt)}</Text>
+                            <Text style={[styles.detalle, styles.textContainer]}>Hora: {formatTime(mov.createdAt)}</Text>
+                        </View>
+                        <View>
+                            {mov.esIngreso ? (
+                                <>
+                                    <Text style={[styles.detalle, styles.textContainer]}>Detalles de Factura</Text>
+                                    <Text style={[styles.detalle, styles.textContainer]}>Tipo de factura: {mov.factura?.esContado ? "Contado" : "Credito"}</Text>
+                                    <Text style={[styles.detalle, styles.textContainer]}>Numero de Factura: {mov.factura?.numeroFactura}</Text>
+                                    <Text style={[styles.detalle, styles.textContainer]}>Fecha de emision de la factura: {mov.factura?.createdAt ? formatDate(mov.factura.createdAt) : ""}</Text>
+                                    <Text style={[styles.detalle, styles.textContainer]}>Monto total a pagar: {Number(mov.factura?.total).toLocaleString()} Gs.</Text>
+                                </>
+                            ) : (
+                                <>
+                                    <Text style={[styles.detalle, styles.textContainer]}>Encargado: {mov.comprobante?.user?.nombre} {mov.comprobante?.user?.apellido}</Text>
+                                    <Text style={[styles.detalle, styles.textContainer]}>RUC: {mov.comprobante?.user?.docIdentidad}</Text>
+                                    <Text style={[styles.detalle, styles.textContainer]}>Observaciones: {mov.comprobante?.concepto}</Text>
+                                </>
+                            )}
+                        </View>
+                    </View>
+                    {mov.esIngreso && mov.movimientoDetalles?.length > 0 && (
+                        <View>
+                            <Text style={[styles.detalle, styles.textContainer]}>Detalles Operacion</Text>
+                            {mov.movimientoDetalles.map((detalle, index) => (
+                                <View key={index} style={[styles.tableRow, { borderBottom: '1px solid #000' }]}>
+                                    <Text style={styles.tableCell}>N°{index + 1}</Text>
+                                    <Text style={styles.tableCell}>Metodo de pago: {detalle.metodoPago}</Text>
+                                    <Text style={styles.tableCell}>Monto Parcial: {Number(detalle.monto).toLocaleString()} Gs.</Text>
+                                </View>
+                            ))}
+                        </View>
+                    )}
                 </View>
             ))}
-            </View>
         </View>
-        </Page>
-    </Document>
+    </Page>
+</Document>
 );
 
 export const generatePDF = async (props: ResumenCajaProps) => {
