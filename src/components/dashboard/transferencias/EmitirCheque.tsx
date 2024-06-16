9e742c2561f837909064110f599ce163f01e112b
Agus3160 <ampharos321@hotmail.com>
2024-06-16 17:58:25 -0400

    Modificacion de formulario de transaccion

--- src/components/dashboard/transferencias/EmitirCheque.tsx
+++ src/components/dashboard/transferencias/EmitirCheque.tsx
@@ -2,175 +2,133 @@ import Input from '@/components/global/Input'
 import InputCalendar from '@/components/global/InputCalendar'
 import React, { useEffect, useState } from 'react'
 import { ChequeCreate } from './AgregarCheque'
-import { Banco } from '@prisma/client'
 import { CuentaBancariaAndBanco } from '@/lib/definitions'
 import { CrearOperacionFields } from '@/lib/moduloBanco/operacion/agregarOperacion'
 
 type Props = {
-  setOperacion: React.Dispatch<React.SetStateAction<CrearOperacionFields>>
-  handleOnChangeOperacion : (event: React.ChangeEvent<HTMLInputElement|HTMLSelectElement>) => void
+  handleOnChange: (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void
   cheques: ChequeCreate[]
   setCheques: React.Dispatch<React.SetStateAction<ChequeCreate[]>>
-  cuentasBancarias: CuentaBancariaAndBanco[]
+  operacion: CrearOperacionFields
 }
 
 export default function EmitirCheque({
-  cuentasBancarias,
-  handleOnChangeOperacion,
-  setOperacion,
-  setCheques
+  handleOnChange,
+  setCheques,
+  operacion,
+  cheques
 }: Props) {
 
-  const [cheque, setCheque] = useState<ChequeCreate>({
-    numeroCheque: '',
-    involucrado: '',
-    monto: 0,
-    fechaEmision: '',
-    esRecibido: false,
-    bancoChequeId: '',
-  })
-  
-  const onChangeHanlder = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCheque(prev => ({ ...prev, [event.target.name]: event.target.value }))
-  }
-
-  useEffect(() => {
-    // setOperacion(prev => ({ ...prev, ["bancoChequeId"]: cheque.bancoChequeId }))
-    setCheques(prev => {
-      const prevList = [...prev]
-      prevList[0] = (cheque)
-      return prevList
-    })
-  }, [cheque])  
+  console.log(cheques)
 
   return (
     <>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-          <label className="mb-2">
-            Cuenta Afectada
-          </label>
-          <div className="relative mt-2">
-            <select
-              className="block appearance-none w-full bg-gray-800 py-3 px-4 pr-8 rounded leading-tight focus:outline-none"
-              onChange={(e) => {
-                handleOnChangeOperacion(e)
-                setCheque(prev => ({ ...prev, ["bancoChequeId"]: cuentasBancarias.find(c => c.id === e.target.value)?.bancoId || "" }))
-              }}
-              id="cuentaBancariaOrigenId"
-              required
-            >
-              
-              {cuentasBancarias.map((cuenta) => (
-                  <option key={cuenta.id} value={cuenta.id}>
-                    {cuenta.banco.nombre.split("Banco")} {cuenta.numeroCuenta}
-                  </option>
-                ))
-              }
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className="flex sm:flex-row flex-wrap flex-col box-border gap-3 mb-3 w-full">
 
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+        <div className="flex flex-col w-full">
           <label className=" mb-2">
             Nombre del Acreedor
           </label>
           <Input
             className="block w-full bg-gray-800 rounded py-3 px-6 my-2 leading-tight focus:outline-none"
             id="nombreInvolucrado"
-            onChange={ (e) =>{ 
-              handleOnChangeOperacion(e)
-              onChangeHanlder(e)
-            }}
+            onChange={
+              (e) => {
+                handleOnChange(e)
+                setCheques(prev => [{ ...prev[prev.length - 1], involucrado: e.target.value }])
+              }
+            }
             type="text"
+            value={operacion.nombreInvolucrado || ""}
             required
-            placeholder="Pedro Meza"
+            placeholder="Ingrese el nombre"
           />
         </div>
 
-        <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-          <label className=" mb-2">
-            Num. Cheque
-          </label>
+        <div className="flex flex-col w-full">
+          <label className="mb-2">Ruc del Acreedor</label>
           <Input
             className="block w-full bg-gray-800 rounded py-3 px-6 my-2 leading-tight focus:outline-none"
-            id="numeroCheque"
-            onChange={ (e) =>{ 
-              handleOnChangeOperacion(e)
-              onChangeHanlder(e)
-            }}
+            id="rucInvolucrado"
+            onChange={handleOnChange}
+            value={operacion.rucInvolucrado || ""}
             type="text"
-            required
-            placeholder="Pedro Meza"
+            placeholder="Ingrese el RUC"
           />
         </div>
-
-        <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-          <label className=" mb-2">Monto</label>
+        
+        <div className="flex flex-col w-full">
+          <label className=" mb-2">N° de Cheque</label>
           <Input
             className={'block w-full bg-gray-800 rounded py-3 px-6 my-2 leading-tight focus:outline-none'}
-            id="monto"
-            onChange={(e) => {
-              handleOnChangeOperacion(e)
-              onChangeHanlder(e)
-            }}
-            type="number"
+            onChange={(e) => setCheques(prev => [{ ...prev[0], numeroCheque: e.target.value }])}
+            id="cuentaInvolucrado"
+            value={cheques.length !== 0 ? cheques[0].numeroCheque : ""}
+            type="text"
             required
-            placeholder="150000"
+            placeholder="Ingrese el numero de cheque"
+          />
+        </div>
+        
+        <div className="flex flex-col w-full">
+          <label className=" mb-2">Monto*</label>
+          <Input
+            className={'block w-full bg-gray-800 rounded py-3 px-6 my-2 leading-tight focus:outline-none'}
+            onChange={(e) => {
+              handleOnChange(e)
+              setCheques(prev => [{ ...prev[0], monto: Number(e.target.value) }])
+            }}
+            id="monto"
+            value={operacion.monto}
+            type="formattedNumber"
+            required
+            placeholder="Ingrese el monto"
           />
         </div>
 
-        <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-          <label className="mb-2">Número de Comprobante</label>
+        <div className="flex flex-col w-full">
+          <label className="mb-2">Número de Comprobante*</label>
           <Input
             className="block w-full bg-gray-800 rounded py-3 px-6 my-2 leading-tight focus:outline-none"
             id="numeroComprobante"
             type="text"
-            onChange={handleOnChangeOperacion}
+            onChange={handleOnChange}
+            value={operacion.numeroComprobante}
             required
-            placeholder="012345"
+            placeholder="Ingrese el numero de comprobante"
           />
         </div>
 
       </div>
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <label className="mb-2">Fecha de la Transacción</label>
+
+      <div className="flex w-full gap-3">
+        <div className="w-full md:  mb-6 md:mb-0">
+          <label className="mb-2">Fecha de la Transacción*</label>
           <InputCalendar
             withTime={true}
             className="block w-full bg-gray-800 rounded py-3 px-6 my-2 leading-tight focus:outline-none"
             id="fechaOperacion"
-            handleChange={
-              (e) => {
-                handleOnChangeOperacion(e)
-                setCheque(prev => ({ ...prev, ["fechaEmision"]: new Date (e.target.value).toISOString() }))
-              }
-            }
+            handleChange={(e) => {
+              handleOnChange(e)
+              setCheques(prev => [{ ...prev[0], fechaEmision: e.target.value }])
+            }}
+            value={operacion.fechaOperacion}
             required
           />
         </div>
-        <div className="w-full md:w-2/3 px-3 mb-6 md:mb-0">
-          <label className="mb-2">Concepto</label>
+        <div className="w-full md:w-2/3 mb-6 md:mb-0">
+          <label className="mb-2">Concepto*</label>
           <Input
             className="block w-full bg-gray-800 rounded py-3 px-6 my-2 leading-tight focus:outline-none"
             id="concepto"
             type="text"
-            onChange={handleOnChangeOperacion}
+            onChange={handleOnChange}
+            value={operacion.concepto}
             required
-            placeholder="Pago de servicios básicos"
+            placeholder="Ingrese el concepto"
           />
         </div>
+
       </div>
     </>
   )
