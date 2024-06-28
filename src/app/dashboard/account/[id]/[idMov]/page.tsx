95c9cc600ed0b9453269d7cd98fbec1ccf50d9b7
AnniaBenitez <anniamicaela@gmail.com>
2024-06-27 20:05:03 -0400

    listo que cada movimiento tenga su propia pantalla de detalle, falto terminar pdfs, ya hago xd

--- src/app/dashboard/account/[id]/[idMov]/page.tsx
+++ src/app/dashboard/account/[id]/[idMov]/page.tsx
@@ -4,11 +4,13 @@ import { useParams, useRouter } from "next/navigation";
 import obtenerOperacionPorId from "../../../../../lib/moduloBanco/operacion/obtenerOperacionPorId";
 import TransferReceipt from "../../../../../components/PDF/TransferenciaDetails";
 import { PDFDownloadLink } from "@react-pdf/renderer";
-import { obtenerCookie } from "@/lib/obtenerCookie";
-import { UserWithName } from "@/lib/definitions";
-import useCookies from "@/lib/hooks/useCookies";
 import LoadingCirleIcon from "@/components/global/LoadingCirleIcon";
 import { useSession } from "next-auth/react";
+import Retiro from "@/components/dashboard/account/TransferenciaDetalles/Retiro";
+import Deposito from "@/components/dashboard/account/TransferenciaDetalles/Deposito";
+import DebitoBancario from "@/components/dashboard/account/TransferenciaDetalles/DebitoBancario";
+import Transferencia from "@/components/dashboard/account/TransferenciaDetalles/Transferencia";
+import EmitirCheque from "@/components/dashboard/account/TransferenciaDetalles/EmitirCheque";
 
 export default function PageComponent() {
   const [dateTime, setDateTime] = useState("undefined");
@@ -24,6 +26,7 @@ export default function PageComponent() {
   const [numCuentaOrigen, setNumCuentaOrigen] = useState("undefined");
   const [bancoOrigen, setBancoOrigen] = useState("undefined");
   const [tipoOperacion, setTipoOperacion] = useState("undefined");
+  const [tipoOperacionNombre, setTipoOperacionNombre] = useState("undefined");
   const [loading, setLoading] = useState(true);
 
   const { data: session, status } = useSession();
@@ -42,7 +45,7 @@ export default function PageComponent() {
       router.push("not-found");
       return;
     }
-
+    
     const fechaHora = new Date(data.data.fechaOperacion);
     const formattedDateTime = fechaHora.toLocaleString("es-ES", {
       day: "2-digit",
@@ -64,11 +67,13 @@ export default function PageComponent() {
     setNumCuentaOrigen(data.data.cuentaBancariaOrigen.numeroCuenta);
     setBancoOrigen(data.data.cuentaBancariaOrigen.banco.nombre);
     setTipoOperacion(data.data.tipoOperacion.esDebito ? "Debito" : "Credito");
+    setTipoOperacionNombre(data.data.tipoOperacion.nombre)
     setLoading(false);
   };
 
   useEffect(() => {
     getTransferencia();
+    console.log(tipoOperacionNombre)
   });
 
   const handleCloseButtonClick = () => {
@@ -83,122 +88,92 @@ export default function PageComponent() {
 
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <div className="w-1/2 py-4 border-b border-gray-300">
-        <h1 className="text-2xl font-bold pb-4">
-          Comprobante de Transferencia - {tipoOperacion}
-        </h1>
-        <div className="flex items-center ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-calendar-clock"
-          >
-            <path d="M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5" />
-            <path d="M16 2v4" />
-            <path d="M8 2v4" />
-            <path d="M3 10h5" />
-            <path d="M17.5 17.5 16 16.3V14" />
-            <circle cx="16" cy="16" r="6" />
-          </svg>
-          <p className="text-lg px-2">Fecha: {dateTime}</p>
-        </div>
-      </div>
-      <div className="w-1/2 py-4 border-b border-gray-300">
-        <div className="flex justify-between w-full">
-          <p className="w-1/3 text-base">Monto:</p>
-          <p className="w-2/3 text-base text-right">
-            {Number(monto).toLocaleString("es-PY")}
-          </p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p className="w-1/3 text-base">Comprobante:</p>
-          <p className="w-2/3 text-base text-right">{numComprobante}</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p className="w-1/3 text-base">Concepto:</p>
-          <p className="w-2/3 text-base text-right">{concepto}</p>
-        </div>
-      </div>
 
-      <div className="w-1/2 pb-4 border-b border-gray-300">
-        <div className="flex items-center border mb-2 px-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-hand-coins"
-          >
-            <path d="M11 15h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 17" />
-            <path d="m7 21 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9" />
-            <path d="m2 16 6 6" />
-            <circle cx="16" cy="9" r="2.9" />
-            <circle cx="6" cy="5" r="3" />
-          </svg>
-          <p className="text-lg px-2">
-            {tipoOperacion == "Debito" ? "Destino" : "Origen"}
-          </p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p className="w-1/3 text-base">Nombre:</p>
-          <p className="w-2/3 text-base text-right">{nombreDestino}</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p className="w-1/3 text-base">Cuenta:</p>
-          <p className="w-2/3 text-base text-right">{numCuentaDestino}</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p className="w-1/3 text-base">Banco:</p>
-          <p className="w-2/3 text-base text-right">{bancoDestino}</p>
-        </div>
-      </div>
-      <div className="w-1/2 pb-4 border-b border-gray-300">
-        <div className="flex items-center border mb-2 px-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-banknote"
-          >
-            <rect width="20" height="12" x="2" y="6" rx="2" />
-            <circle cx="12" cy="12" r="2" />
-            <path d="M6 12h.01M18 12h.01" />
-          </svg>
-          <p className="text-lg px-2">
-            {tipoOperacion == "Debito" ? "Origen" : "Destino"}
-          </p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p className="w-1/3 text-base">Nombre:</p>
-          <p className="w-2/3 text-base text-right">{nombreOrigen}</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p className="w-1/3 text-base">Cuenta:</p>
-          <p className="w-2/3 text-base text-right">{numCuentaOrigen}</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p className="w-1/3 text-base">Banco:</p>
-          <p className="w-2/3 text-base text-right">{bancoOrigen}</p>
-        </div>
-      </div>
+      {
+        tipoOperacionNombre == "Retiro" ? <Retiro
+          tipoOperacion={tipoOperacion}
+          tipoOperacionNombre={tipoOperacionNombre}
+          dateTime={dateTime}
+          monto={monto}
+          numComprobante={numComprobante}
+          concepto={concepto}
+          nombreDestino={nombreDestino}
+          numCuentaDestino={numCuentaDestino ? numCuentaDestino : ""}
+          bancoDestino={bancoDestino ? bancoDestino : ""}
+          nombreOrigen={nombreOrigen}
+          numCuentaOrigen={numCuentaOrigen}
+          bancoOrigen={bancoOrigen}
+        /> : null}
+
+
+      {
+        tipoOperacionNombre == "Depósito" ? <Deposito
+          tipoOperacion={tipoOperacion}
+          tipoOperacionNombre={tipoOperacionNombre}
+          dateTime={dateTime}
+          monto={monto}
+          numComprobante={numComprobante}
+          concepto={concepto}
+          nombreDestino={nombreDestino}
+          numCuentaDestino={numCuentaDestino ? numCuentaDestino : ""}
+          bancoDestino={bancoDestino ? bancoDestino : ""}
+          nombreOrigen={nombreOrigen}
+          numCuentaOrigen={numCuentaOrigen}
+          bancoOrigen={bancoOrigen}
+        /> : null
+      }
+
+      {
+        tipoOperacionNombre == "Débito bancario" ? <DebitoBancario
+          tipoOperacion={tipoOperacion}
+          tipoOperacionNombre={tipoOperacionNombre}
+          dateTime={dateTime}
+          monto={monto}
+          numComprobante={numComprobante}
+          concepto={concepto}
+          nombreDestino={nombreDestino}
+          numCuentaDestino={numCuentaDestino ? numCuentaDestino : ""}
+          bancoDestino={bancoDestino ? bancoDestino : ""}
+          nombreOrigen={nombreOrigen}
+          numCuentaOrigen={numCuentaOrigen}
+          bancoOrigen={bancoOrigen}
+        /> : null
+      }
+
+      {
+        tipoOperacionNombre == "Transferencia (Emitida)" || tipoOperacionNombre == "Transferencia (Recibida)" ? <Transferencia
+          tipoOperacion={tipoOperacion}
+          tipoOperacionNombre={tipoOperacionNombre}
+          dateTime={dateTime}
+          monto={monto}
+          numComprobante={numComprobante}
+          concepto={concepto}
+          nombreDestino={nombreDestino}
+          numCuentaDestino={numCuentaDestino ? numCuentaDestino : ""}
+          bancoDestino={bancoDestino ? bancoDestino : ""}
+          nombreOrigen={nombreOrigen}
+          numCuentaOrigen={numCuentaOrigen}
+          bancoOrigen={bancoOrigen}
+        /> : null
+      }
+
+      {
+        tipoOperacionNombre == "Emitir Cheque" ? <EmitirCheque
+          tipoOperacion={tipoOperacion}
+          tipoOperacionNombre={tipoOperacionNombre}
+          dateTime={dateTime}
+          monto={monto}
+          numComprobante={numComprobante}
+          concepto={concepto}
+          nombreDestino={nombreDestino}
+          numCuentaDestino={numCuentaDestino ? numCuentaDestino : ""}
+          bancoDestino={bancoDestino ? bancoDestino : ""}
+          nombreOrigen={nombreOrigen}
+          numCuentaOrigen={numCuentaOrigen}
+          bancoOrigen={bancoOrigen}
+        /> : null
+      }
+
       <div className="w-1/2 py-4 border-gray-300">
         <div className="flex flex-row">
           <svg
